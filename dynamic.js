e94f777ed4ec5d2ed5f1b02a854fd211430ee728
Naomi <naomiclark2004@gmail.com>
2024-12-27 12:49:32 -0700

    fixed tablet/mobile sizing

--- dynamic.js
+++ dynamic.js
@@ -10,71 +10,70 @@ myList = JSON.parse(localStorage.getItem("myList"));
 jsloader();
 
 function jsloader() {
-    fetch(url)
-        .then((rep) => rep.json())
-        .then((data) => {
-            myList = data;
-            maker();
-            savetoStorage();
-            dynamic();
-        });
+  fetch(url)
+    .then((rep) => rep.json())
+    .then((data) => {
+      myList = data;
+      maker();
+      savetoStorage();
+      dynamic();
+    });
 }
 
 // so maker doesn't run on second page
 function maker() {
-    if (document.body.contains(output)) {
-        output.innerHTML = " ";
-        myList.forEach((el, index) => {
-            makeList(el, index);
-        });
-    }
-    else {
-        dynamic();
-    }
+  if (document.body.contains(output)) {
+    output.innerHTML = " ";
+    myList.forEach((el, index) => {
+      makeList(el, index);
+    });
+  } else {
+    dynamic();
+  }
 }
 
 // make each attraction content box
 function makeList(item, index) {
-    const box = document.createElement('div');
-    box.className = "box";
-    const div = document.createElement('div');
-    div.className = "text";
-    const img = document.createElement("img");
-    img.src = `${item.image}`;
-    img.style.width = "300px";
-    img.className = "image";
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    h2.innerHTML = (index + 1) + ".  " + item.name;
-    p.innerHTML = `${item.description}`;
-    output.append(box);
-    box.append(img);
-    box.append(div);
-    div.append(h2);
-    div.append(p);
-    savetoStorage();
+  const box = document.createElement("div");
+  box.className = "box";
+  const div = document.createElement("div");
+  div.className = "text";
+  const img = document.createElement("img");
+  img.src = `${item.image}`;
+  // img.style.width = "300px";
+  img.className = "image";
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
+  h2.innerHTML = index + 1 + ".  " + item.name;
+  p.innerHTML = `${item.description}`;
+  output.append(box);
+  box.append(img);
+  box.append(div);
+  div.append(h2);
+  div.append(p);
+  savetoStorage();
 }
 
 function savetoStorage() {
-    //   console.log(myList);
-    localStorage.setItem("myList", JSON.stringify(myList));
+  //   console.log(myList);
+  localStorage.setItem("myList", JSON.stringify(myList));
 }
 
 // stores img elements in as a collection then use loop to add eventlistner to each to open second web page
 function dynamic() {
-    var images = document.getElementsByClassName("image");
-    //   console.log(images);
-    for (let i = 0; i < images.length; i++) {
-        images[i].addEventListener("click", function () {
-            if (images[i] === images[i]) {
-                openInNewTab('locationMap.html');
-                localStorage.setItem("map", i);
-            }
-        });
-    }
+  var images = document.getElementsByClassName("image");
+  //   console.log(images);
+  for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", function () {
+      if (images[i] === images[i]) {
+        openInNewTab("locationMap.html");
+        localStorage.setItem("map", i);
+      }
+    });
+  }
 }
 // open map page when image is clicked
 function openInNewTab(url) {
-    const open = window.open(url, '_blank');
-    open.focus();
+  const open = window.open(url, "_blank");
+  open.focus();
 }
--- dynamic2.js
+++ dynamic2.js
@@ -8,35 +8,35 @@ var i = Number(item);
 var sidebar = document.querySelector(".sidebar");
 var map = document.querySelector(".map");
 
-// make sidebar content box and map display 
+// make sidebar content box and map display
 function makeMap() {
-    var item = myList[i];
-    var img = document.createElement('img');
-    var content = document.createElement('div');
-    content.className="content";
-    var title = document.createElement('h2');
-    var address = document.createElement('div');
-    address.className="address";
-    var ad = document.createElement('p');
-    const p = document.createElement('p');
-    p.innerHTML = `${item.description}`;
-    img.src = `${item.image}`;
-    img.style.width = "400px";
-    img.id="item-image";
-    title.innerHTML = `${item.name}`;
-    var add = document.createElement('img');
-    add.src="placeholder.png";
-    add.style.width = "20px";
-    add.style.height= "20px";
-    ad.innerHTML = item.address;
-    sidebar.append(img);
-    sidebar.append(content);
-    content.append(title);
-    content.append(address);
-    address.append(add);
-    address.append(ad);
-    content.append(p);
+  var item = myList[i];
+  var img = document.createElement("img");
+  var content = document.createElement("div");
+  content.className = "content";
+  var title = document.createElement("h2");
+  var address = document.createElement("div");
+  address.className = "address";
+  var ad = document.createElement("p");
+  const p = document.createElement("p");
+  p.innerHTML = `${item.description}`;
+  img.src = `${item.image}`;
+  // img.style.width = "400px";
+  img.id = "item-image";
+  title.innerHTML = `${item.name}`;
+  var add = document.createElement("img");
+  add.src = "placeholder.png";
+  //   add.style.width = "20px";
+  //   add.style.height = "20px";
+  ad.innerHTML = item.address;
+  sidebar.append(img);
+  sidebar.append(content);
+  content.append(title);
+  content.append(address);
+  address.append(add);
+  address.append(ad);
+  content.append(p);
 
-    map.innerHTML=`<iframe src="${item.map}"></iframe>`;
+  map.innerHTML = `<iframe src="${item.map}"></iframe>`;
 }
 makeMap();
